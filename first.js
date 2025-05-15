439afe7a2ad9217009c749aa861eccefc42f3707
Ritick Raj <riticksharmadt1@gmail.com>
2025-05-16 00:20:13 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,82 @@
+/**console.log("Welcome to Apna College");
+console.log("Student Bihar Board");
+console.log("I love js")**/
+
+
+ /**let fullName = "Virat Kohli";
+ let  age = "36";
+ let totalPrice = "23";
+console.log(fullName);
+console.log(age);
+console.log(totalPrice);
+
+const PI = "3.14"
+console.log(PI);
+
+ let a;
+ a = 15;
+ console.log(a);
+
+ const a = "10";
+ a = "15
+
+ {
+    let a = "10";
+    console.log(a);
+ }
+ 
+ {
+    let a = "15";
+    console.log(a);
+ }**/
+
+    /** let a = 9.99
+    let price = 100.5
+ isFollow = true;
+let name = "Tony Stark";
+ let age = 18
+ let x;
+ let x = null;
+ let x = BigInt("123");
+ let y = Symbol("Hello World")
+
+
+ const student = {
+    fullName: "Ritick Raj",
+    age: 24,
+    cgpa: 9.8,
+    isPass: true,
+
+ };
+
+ student["name"] = "Ritick Sharma";
+ console.log(student.name);
+
+ const product = {
+    title: "Ball Pen",
+    rating: 4,
+    offer: 5,
+    price: 270,
+ }
+
+ console.log(product);
+ const profile = {
+    username: "@shradhakhapra",
+    isFollow: true,
+    post: 195,
+    followers: 569,
+    following: 4,
+ }
+ console.log(typeof profile["username"]);**/
+
+ const Shop = {
+   title: "Laptop",
+   rating: 4,
+   price: 75000,
+   address: "Dakbunglow Fraser Road",
+ };
+
+ Shop["title"] = "Computer";
+ console.log(Shop.title);
+
+ 
